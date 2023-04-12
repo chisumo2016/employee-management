8178a20b775808bcad6114cf15fa72928fc3f894
bernard chisumo <bchisumo74@gmail.com>
2023-04-12 14:30:10 +0100

    Installed Dashboard Theme

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,24 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                //'resources/sass/app.scss',
+                //'resources/js/app.js',
+                /** CSS FILES*/
+                'resources/assets/admin/vendor/fontawesome-free/css/all.min.css',
+                'resources/assets/admin/css/sb-admin-2.min.css',
+
+                 /**JS FILES*/
+
+                'resources/assets/admin/vendor/jquery/jquery.min.js',
+                'resources/assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js',
+                'resources/assets/admin/vendor/jquery-easing/jquery.easing.min.js',
+                'resources/assets/admin/js/demo/sb-admin-2.min.js',
+                'resources/assets/admin/vendor/chart.js/Chart.min.js',
+
+                'resources/assets/admin/js/demo/chart-area-demo.js',
+                'resources/assets/admin/js/demo/chart-pie-demo.js',
+
+
             ],
             refresh: true,
         }),
